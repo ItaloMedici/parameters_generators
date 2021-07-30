f44e7fe849e4d6e0782d701072eff7a9b2aeb644
ItaloMedici <italomedice969@gmail.com>
2021-07-30 15:17:59 -0300

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+const frase = "Filtrar Data de Nota";
+const param = "P_"; 
+const field = "F_";
+const variable = "V_";
+
+const result = variable + frase.toUpperCase().replace(/ /g, "_");
+console.log(result);
