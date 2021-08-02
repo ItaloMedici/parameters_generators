e1c092fefa3b9fd660060119ef5fd5fce690214c
ItaloMedici <italomedice969@gmail.com>
2021-08-02 16:39:22 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-const frase = "Filtrar Data de Nota";
-const param = "P_"; 
-const field = "F_";
-const variable = "V_";
+const format = (choice) => {
+    const result = document.getElementById("result");
+    const text = document.querySelector(".desc").value;
+    result.innerHTML = choice + text.toUpperCase().replace(/ /g, "_");
+}   
 
-const result = variable + frase.toUpperCase().replace(/ /g, "_");
-console.log(result);
+const copiar = () => {
+    const text = document.getElementById("result").innerHTML ;
+    const alert = document.getElementById("alert");
+    if(text !== '') {
+        navigator.clipboard.writeText(text);
+        showAlert(alert);
+    }
+} 
+
+
+const showAlert = (element) => {
+    setTimeout(() => {
+        element.style.display = 'block';
+    }, 1000);
+    element.style.display = 'none';
+}
